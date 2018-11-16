3f43c90f8202c66900574cde3a2002b9d6276103
Vinicius Arashiro <vinicius.arashiro@gmail.com>
2018-11-16 00:58:40 -0400

    Reason: 	Notification tested and working, and timePcker implemented Changes to be committed: 	modified:   EPrescription/.idea/caches/build_file_checksums.ser 	modified:   EPrescription/app/src/main/java/eprescription/tcc/ufam/com/e_prescription/Activities/TakeMedicineActivity.java 	modified:   EPrescription/app/src/main/res/layout/calendar_popup.xml 	new file:   EPrescription/app/src/main/res/layout/clock_popup.xml 	modified:   EPrescription/app/src/main/res/values/strings.xml

--- EPrescription/app/src/main/java/eprescription/tcc/ufam/com/e_prescription/Activities/TakeMedicineActivity.java
+++ EPrescription/app/src/main/java/eprescription/tcc/ufam/com/e_prescription/Activities/TakeMedicineActivity.java
@@ -1,10 +1,14 @@
 package eprescription.tcc.ufam.com.e_prescription.Activities;
 
+import android.annotation.TargetApi;
+import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.TimePickerDialog;
 import android.content.Context;
 import android.os.Build;
 import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -15,6 +19,7 @@ import android.widget.DatePicker;
 import android.widget.TextView;
 import android.widget.TimePicker;
 
+import java.sql.Time;
 import java.util.Calendar;
 
 import eprescription.tcc.ufam.com.e_prescription.R;
@@ -24,12 +29,15 @@ public class TakeMedicineActivity extends AppCompatActivity {
     private static final String TAG = "TAKEMEDICINE";
     private Button setDate;
     private DatePicker datePicker;
+    private TimePicker timePicker;
     private Button okButton;
     private TextView initialDay;
     private TextView initialHour;
-    private Button timePicker;
+    private Button setTime;
     private AlertDialog calendarDialog;
     private AlertDialog.Builder calendarDialogBuilder;
+    private AlertDialog clockDialog;
+    private AlertDialog.Builder clockDialogBuilder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,13 +46,20 @@ public class TakeMedicineActivity extends AppCompatActivity {
 
         initialDay = (TextView) findViewById(R.id.initialDayID);
         initialHour = (TextView) findViewById(R.id.timeTextID);
-        timePicker = (Button) findViewById(R.id.timePickerBtn);
+        setTime = (Button) findViewById(R.id.timePickerBtn);
         setDate = (Button) findViewById(R.id.pickDateBtnID);
 
         setDate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 createCalendarPopUp();
+            }
+        });
+
+        setTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createClockPopUp();
             }
         });
     }
@@ -71,24 +86,55 @@ public class TakeMedicineActivity extends AppCompatActivity {
 
     }
 
-    private void simpleNotification(View view) {
-        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this)
-                .setSmallIcon(R.drawable.ic_notifications_black_24dp)
-                .setContentTitle("Test")
-                .setAutoCancel(true)
-                .setContentText("Test")
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
-        Log.d(TAG, "user signed in");
+    private void createClockPopUp() {
+        clockDialogBuilder = new AlertDialog.Builder(this);
+        final View view = getLayoutInflater().inflate(R.layout.clock_popup, null);
+        timePicker = (TimePicker) view.findViewById(R.id.timePickerID);
+        clockDialogBuilder.setView(view);
+        clockDialog = clockDialogBuilder.create();
+        clockDialog.show();
 
-        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            NotificationChannel channel = new NotificationChannel("notify_001",
-                    "Channel human readable title",
-                    NotificationManager.IMPORTANCE_DEFAULT);
-            notificationManager.createNotificationChannel(channel);
-        }
-        notificationManager.notify(0, mBuilder.build());
+        Calendar calendar = Calendar.getInstance();
+        int hour = calendar.get(Calendar.HOUR_OF_DAY);
+        int mMinute = calendar.get(Calendar.MINUTE);
+
+        TimePickerDialog timePickerDialog = new TimePickerDialog(this, new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                initialHour.setText(hourOfDay + ":" + minute);
+            }
+        }, hour, mMinute, true);
+        timePickerDialog.show();
     }
 
+    private void simpleNotification(View view) {
+
+        String CHANNEL_ID = "my_channel";
+        createNotificationChannel(CHANNEL_ID);
+        int notificationID = 1;
+
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_notifications_black_24dp)
+                .setContentTitle("Test")
+                .setContentText("Test")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+        notificationManager.notify(notificationID, mBuilder.build());
+        Log.d(TAG, "user signed in");
+        }
+
+    private void createNotificationChannel(String channel_id) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = getString(R.string.channel_name);
+            String description = getString(R.string.channel_description);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(channel_id, name, importance);
+            channel.setDescription(description);
+
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
     // TODO TIMEPICKER
 }
